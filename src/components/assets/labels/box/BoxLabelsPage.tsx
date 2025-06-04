fb1c2ed0ba2b45305961247521c05c6edb003bae
Tsmart <info@tsmart.ai>
2025-06-05 00:14:07 +0300

    Add default export to BoxLabelsPage component

--- src/components/assets/labels/box/BoxLabelsPage.tsx
+++ src/components/assets/labels/box/BoxLabelsPage.tsx
@@ -1,1 +1,12 @@
-// Content shown in previous file changes
+import React from 'react';
+
+const BoxLabelsPage = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Box Labels</h1>
+      {/* Add your box labels content here */}
+    </div>
+  );
+};
+
+export default BoxLabelsPage;
