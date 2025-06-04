d5e218c091037e734aceec1c9812c635fe50b033
Tsmart <info@tsmart.ai>
2025-06-04 23:06:52 +0300

    Fix Navigate path in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -77,7 +77,7 @@ function App() {
           </Route>
         ) : (
           // Redirect to landing page if not authenticated
-          <Route path="*" element={<Navigate to="/\" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         )}
       </Routes>
     </Router>
