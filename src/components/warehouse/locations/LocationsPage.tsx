fd2dcd4c146f7f9a8cfcd411c787c5ddd08ab9fb
Tsmart <info@tsmart.ai>
2025-06-04 23:25:58 +0300

    Fix Card component import

--- src/components/warehouse/locations/LocationsPage.tsx
+++ src/components/warehouse/locations/LocationsPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from '../../ui/Card';
+import Card from '../../ui/Card';
 import Button from '../../ui/Button';
 import { Input } from '../../ui/Input';
 import { Plus, Search } from 'lucide-react';
