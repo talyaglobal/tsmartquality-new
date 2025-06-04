1bb54b945fc88fd561fbf3e8df9597255e235635
Tsmart <info@tsmart.ai>
2025-06-04 23:24:44 +0300

    Fix Button component import

--- src/components/warehouse/locations/LocationsPage.tsx
+++ src/components/warehouse/locations/LocationsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card } from '../../ui/Card';
-import { Button } from '../../ui/Button';
+import Button from '../../ui/Button';
 import { Input } from '../../ui/Input';
 import { Plus, Search } from 'lucide-react';
 
